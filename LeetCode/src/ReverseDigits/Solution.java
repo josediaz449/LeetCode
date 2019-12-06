3a633b288578bf4a46b17330e7ef474b2981e6ac
josediaz449 <48035985+josediaz449@users.noreply.github.com>
2019-12-06 02:40:04 -0500

    Finished reverse digits

--- LeetCode/src/ReverseDigits/Solution.java
+++ LeetCode/src/ReverseDigits/Solution.java
@@ -1,10 +1,38 @@
 package ReverseDigits;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Solution {
    public int reverse(int x) {
       int result=0;
+      Boolean negative=false;
+      String sx = Integer.toString(x);
+      Stack<String> nums = new Stack<>();
+      for(int i = 0; i< sx.length();i++){
+         if(x<0&&i==0){
+            negative=true;
+         }
+         else{
+            nums.push(Character.toString(sx.charAt(i)));
+         }
+      }
+      String sxs="";
+      while(!nums.isEmpty()){
+         sxs+=nums.pop();
+      }
+      //System.out.println(sxs);
+
+      try {
+         result=Integer.parseInt(sxs);
+      }
+      catch (NumberFormatException e){
+         return 0;
+      }
+      if(negative){
+         result=result*-1;
+      }
+
 
 
       return result;
